af768679fc9f3fbee04e84956a5673ddb020fbdb
Anna Parissi <anna.parissi@persado.com>
2023-06-20 00:26:17 +0300

    Add cache for BINTable api results

--- src/main/java/com/example/cardcost/service/BinTableApiService.java
+++ src/main/java/com/example/cardcost/service/BinTableApiService.java
@@ -2,6 +2,7 @@ package com.example.cardcost.service;
 
 import com.example.cardcost.dto.BinApiResponseDto;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,6 +18,8 @@ public class BinTableApiService {
     @Value("${api.bintable.api_key}")
     String binTableApiKey;
 
+
+    @Cacheable(value = "binapi-results-cache", key = "#issuerIdentificationNumber", unless="#result == null")
     public String getCountryCodeFromBinTableApi(String issuerIdentificationNumber) {
 
         Map<String, String> map = new HashMap<>();
@@ -29,6 +32,5 @@ public class BinTableApiService {
         if (response == null)
             throw new RuntimeException("Response received from BinTable api is null");
         return response.getData().getCountry().getCode().toLowerCase();
-
     }
 }
